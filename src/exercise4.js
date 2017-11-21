535b891fe52e4ba6328de60934076ff371b80a92
unknown <killian.barreau@ynov.com>
2017-11-21 16:51:35 +0100

    Exercice 4 terminé

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,7 +1,18 @@
 exports.run = function(input) {
-  console.log("Exercise #4");
+  function repeat(nbr, string) {
+    var stringArray = [];
+    var result = "";
 
-  // YOUR CODE IS WELCOME HERE
+    for(i = 0; i<nbr; i++) {
+      stringArray.push(string);
+    }
+    result = stringArray.join(", ")
+    return result;
+  }
+  
+  function input(nbr, string, callback) {
+    return callback(nbr, string);
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
